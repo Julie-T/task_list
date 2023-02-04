ba0b23cd29c01c415befffe9af228a2f99046df2
Juliya <yula_sakura@list.ru>
2023-02-05 02:42:03 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -2,31 +2,20 @@ import React, {Component} from "react";
 import Task from "./components/Task";
 import TaskInput from "./components/TaskInput";
 import {nanoid} from 'nanoid'
+import {observer} from 'mobx-react'
+import store from "./store/index";
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            tasks: [
-                {id: 0, title: 'walk the dog', done: false,},
-                {id: 1, title: 'car wash', done: true,},
-                {id: 2, title: 'housework', done: true,},
-                {id: 3, title: 'pink shorts', done: false,},
-            ]
-        }
-    }
-
-    addTask = (task) => {
-        const newTask = {
-            id: this.state.tasks.length !== 0 ? task.length : 0,
-            title: task,
-            done: false,
-        }
-        const newTasks = this.state.tasks.push(newTask)
-        this.setState( {task: newTasks})
-
+    // addTask = (task) => {
+    //     const newTask = {
+    //         id: this.state.tasks.length !== 0 ? task.length : 0,
+    //         title: task,
+    //         done: false,
+    //     }
+    //     const newTasks = this.state.tasks.push(newTask)
+    //     this.setState({task: newTasks})
+    // }
         // this.setState(state => {
         //     let {tasks} = state;
         //     tasks.push({
@@ -36,9 +25,8 @@ class App extends Component {
         //     });
         //     return tasks;
         // })
-    }
 
-    doneTask = (id) => {
+    // doneTask = (id) => {
         // const index = this.state.tasks.map(task => task.id).indexOf(id);
         // this.setState(state => {
         //     let { tasks } = state;
@@ -49,40 +37,42 @@ class App extends Component {
         //     tasks[index].done = !tasks[index].done;
         //     return tasks;
         // })
-        const newTasks = this.state.tasks.map(task => task.id === id ? task.done = !task.done : task)
-        this.setState({task: newTasks})
-    }
+    //     const newTasks = this.state.tasks.map(task => task.id === id ? task.done = !task.done : task)
+    //     this.setState({task: newTasks})
+    // }
 
-    deleteTask = (id) => {
-        const newTasks = this.state.tasks.filter(task => task.id !== id)
-        this.setState({tasks: newTasks})
+    // deleteTask = (id) => {
+    //     const newTasks = this.state.tasks.filter(task => task.id !== id)
+    //     this.setState({tasks: newTasks})
+    // }
         // const index = this.state.tasks.map(task => task.id).indexOf(id);
         // this.setState(state => {
         //     let {tasks} = state;
         //     delete tasks[index];
         //     return tasks;
         // })
-    }
 
     render() {
         // const {tasks} = this.state;
-        const activeTasks = this.state.tasks.filter(task => !task.done);
-        const doneTasks = this.state.tasks.filter(task => task.done);
+        // const activeTasks = this.state.tasks.filter(task => !task.done);
+        // const doneTasks = this.state.tasks.filter(task => task.done);
 
         return (
             <div className='App'>
-                <h1 className='top'>Active tasks: {activeTasks.length}</h1>
-                {[...activeTasks, ...doneTasks].map(task => (
+                <h1 className='top'>Active tasks: { store.activeTasks.length }</h1>
+                {store.sortedTasks.map(task => (
                     <Task
-                        doneTask={() => this.doneTask(task.id)}
-                        deleteTask={() => this.deleteTask(task.id)}
+                        doneTask={() => store.doneTask(task.id)}
+                        deleteTask={() => store.deleteTask(task.id)}
                         task={task}
                         key={nanoid()}/>
                 ))}
-                <TaskInput addTask={this.addTask}/>
+                <TaskInput addTask={v => store.addTask(v)}/>
             </div>
         );
     }
 }
 
-export default App;
+export default observer(App);
+// метод observer нужен, чтобы приложение ререндерилось
+// при изменении состояния
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,65 @@
-class Store {
+import decorate, {  observable, computed, action } from 'mobx';
 
+class Store {
+    tasks = [
+        {id: 0, title: 'walk the dog', done: false,},
+        {id: 1, title: 'car wash', done: true,},
+        {id: 2, title: 'housework', done: true,},
+        {id: 3, title: 'pink shorts', done: false,},
+    ];
+
+    setTask(payload) {
+        this.tasks = payload;
+    }
+
+    get sortedTasks() {
+        return this.tasks.slice()
+            .sort((a,b) => (a.done === b.done ? 0 : a.done ? 1 : -1));
+    }
+
+    get activeTasks() {
+        return this.tasks.filter(task => !task.done);
+    }
+
+    addTask(task) {
+        let tasks = this.tasks
+        const newTask = {
+            // id: this.tasks.length !== 0 ? task.length : 0,
+            id: this.tasks.length || 0,
+            title: task,
+            done: false,
+        }
+        tasks.push(newTask)
+        // this.setState( {task: newTasks})
+        this.setTask(tasks);
+    }
+
+    doneTask(id) {
+        const newTasks = this.tasks.map(task => task.id === id ? task.done = !task.done : task)
+        // this.setState({task: newTasks})
+        this.setTask(newTasks);
+    }
+    deleteTask(id) {
+        const newTasks = this.tasks.filter(task => task.id !== id)
+        this.setTask(newTasks)
+    }
 }
 
-export default new Store()
+// {activeTasks.length}
+
+// const StoreView = observer(({store})=> (
+//     <span></span>
+// ))
+
+Store = decorate(Store, {
+    tasks: observable,
+    activeTasks: computed,
+    sortedTasks: computed,
+    addTask: action,
+    deleteTask: action,
+    doneTask: action,
+})
+
+const myStore = new Store();
+
+export default myStore
